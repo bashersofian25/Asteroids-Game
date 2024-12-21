3c746a458d98a3f9e031a614f87b05f37697624c
bashersofian25 <bsofian4@gmail.com>
2024-12-21 19:14:12 +0300

    Refresh rate at 60 fps

--- main.py
+++ main.py
@@ -11,12 +11,15 @@ def main():
     print (f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+    dt = 0
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        dt = clock.tick()/1000
         pygame.display.flip()
 
 if __name__ == "__main__":
