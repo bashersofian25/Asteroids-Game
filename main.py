9d30372ead8f8ca0bf86f07e9e9477cd9661a3bb
bashersofian25 <bsofian4@gmail.com>
2024-12-21 21:32:22 +0300

    player class

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -14,11 +15,14 @@ def main():
     dt = 0
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player1 = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    player1.draw(screen)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        player1.draw(screen)
         dt = clock.tick()/1000
         pygame.display.flip()
 
